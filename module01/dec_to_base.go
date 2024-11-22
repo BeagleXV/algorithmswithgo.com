80fa169089e0009ded1cd53ea7323c6514cfe73b
BeagleXV <beaglexv@gmail.com>
2024-11-22 12:43:57 -0300

    DecToBase solved

--- module01/dec_to_base.go
+++ module01/dec_to_base.go
@@ -6,9 +6,37 @@ package module01
 //
 // Eg:
 //
-//   DecToBase(14, 16) => "E"
-//   DecToBase(14, 2) => "1110"
-//
+//	DecToBase(14, 16) => "E"
+//	DecToBase(14, 2) => "1110"
 func DecToBase(dec, base int) string {
-	return ""
+	const charset = "0123456789ABCDEF"
+	var res string
+	for dec > 0 {
+		rem := dec % base
+		res = string(charset[rem]) + res
+		dec = dec / base
+	}
+	return res
 }
+
+/*func DecToBase(dec, base int) string {
+	var rd []int
+	for dec > 0 {
+		n := dec % base
+		rd = append(rd, n)
+		dec /= base
+	}
+
+	for i, j := 0, len(rd)-1; i < j; i, j = i+1, j-1 {
+		rd[i], rd[j] = rd[j], rd[i]
+	}
+
+	symbols := "0123456789ABCDEF"
+	var result string
+
+	for _, digits := range rd {
+		result += string(symbols[digits])
+	}
+
+	return result
+}*/
